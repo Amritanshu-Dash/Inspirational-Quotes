1164b5aab71f24cbfab698cf3563f2a402baef0a
amritanshu_dash <amritanshudash6677@gmail.com>
2023-08-29 19:51:27 +0530

    Initial Commit

---
+++ Inspirational Quotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Inspirational Quotes
+//
+//  Created by Amritanshu Dash on 29/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
